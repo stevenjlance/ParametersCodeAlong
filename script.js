74365dec9d9cc149492090e51c55031412bdfa59
Steven Lance <slance@democracyprep.org>
2021-01-14 20:51:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// 1. Create a function called printHello() that prints "Hello!" to the console when called. Call the function 4 times
+
+
+// 2. Create a function called addTwoAndThree that prints out the result of 2 + 3. Call the function twice
+
+
+
+// 3. complementBot(): A function called complementBot has been defined below. Call this function below the definition and pass in the value of your name in quotes! 
+function complementBot(name) {
+  console.log(`${name} is the best!`);
+}
+
+
+
+
+// 4. insultBot(): Define a function called insultBot() that:
+    //a. Accepts a name as a parameter
+    //b. Prints out an insult when the function is called
+    //c. Call the function with 3 different names
+
+
+
+
+// 5. monthToSeason: Create a funcation called monthToSeason that accepts a month and prints out the corresponding season. For example, if a January is input, then "It is winter" should be printed out.
+
+
+
+// 6. randomGreeting: Create a function called randomGreeting that takes in a persons name. The function should print out a random greeting from the greetings list each time it is called. Call the function 4 times to test this out!
+var greetings = ["Good day,", "Bonne journée,", "Buenos dias,", "美好的一天,", "Merhbe,", "مرحبا,", "여보세요, "];
